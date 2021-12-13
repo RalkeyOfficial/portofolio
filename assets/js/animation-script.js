16269caa702ddc27dbdc91d8ee46aa43792fca8c
RalkeyOfficial <Ralkey@outlook.com>
2021-12-13 14:41:32 +0100

    fixed random bg, made blue hook page, added new animation to languages

--- assets/js/animation-script.js
+++ assets/js/animation-script.js
@@ -6,78 +6,83 @@ setInterval(function() {
         'background-position-y': (scroll * 0.6) + 'px'
         //          change it to -(scroll * num) to make it go faster than actual scroll
     });
-}, 10);
+}, 8);
 
 
 // expand header on element load
-$('.header').ready(function() {
-    $('.header').css({
-        width: '0'
-    });
-
-    $('.header').delay(500).animate({
-        width: '45em'
-    }, 1000, 'easeInOutQuart');
+$('.header').css({
+    width: '0'
 });
+$('.header').delay(500).animate({
+    width: '45em'
+}, 1000, 'easeInOutQuart');
+
 
 // fade in small-about-me class from left on element load
-$('.small-about-me').children().first().ready(function() {
-    $('.small-about-me').children().first()
-    .css({
-        'left': '0',
-        opacity: '0'
-    });
+$('.small-about-me').children().first()
+.css({
+    'left': '0',
+    opacity: '0'
+});
+$('.small-about-me').children().first()
+.delay(1500).animate({
+    'left': '15%',
+    opacity: '1'
+}, 2000, 'easeOutQuart');
 
-    $('.small-about-me').children().first()
-    .delay(1500).animate({
-        'left': '15%',
+
+//      [[---  about me animation  ---]]
+const aboutMe = $('.about-me').children().first();
+aboutMe.css({
+    'left': '100%',
+    opacity: '0'
+});
+aboutMe.waypoint(function() {
+    aboutMe.animate({
+        'left': '50%',
         opacity: '1'
     }, 2000, 'easeOutQuart');
+
+}, { offset : '75%' });
+
+
+//      [[---  showcase animation  ---]]
+const showcase = $('.showcase-field').children().first();
+showcase.css({
+    'left': '0',
+    opacity: '0'
 });
+showcase.waypoint(function() {
+    showcase.animate({
+        'left': '50%',
+        opacity: '1'
+    }, 2000, 'easeOutQuart');
+}, { offset: '75%' });
 
-// fade in slide-show class from right when in view
-$(document).ready(function() {
-    //      [[---  about me animation  ---]]
-    const aboutMe = $('.about-me').children().first();
-    aboutMe.css({
-        'left': '100%',
-        opacity: '0'
-    });
 
-    aboutMe.waypoint(function() {
-        aboutMe.animate({
-            'left': '50%',
-            opacity: '1'
-        }, 2000, 'easeOutQuart');
-
-    }, { offset : '85%' });
-
-    //      [[---  showcase animation  ---]]
-    const showcase = $('.showcase-field').children().first();
-    showcase.css({
-        'left': '0',
-        opacity: '0'
-    });
-
-    showcase.waypoint(function() {
-        showcase.animate({
-            'left': '50%',
-            opacity: '1'
-        }, 2000, 'easeOutQuart');
-    }, { offset: '75%' });
-
-    //      [[---  languages animation  ---]]
-    const languages = $('.languages').children().first();
-    languages.css({
-        'left': '100%',
-        opacity: '0'
-    });
-
-    languages.waypoint(function() {
-        languages.animate({
-            'left': '50%',
-            opacity: '1'
-        }, 2000, 'easeOutQuart');
-
-    }, { offset : '85%' });
+//      [[---  languages animation  ---]]
+const languages = $('.languages').children().first();
+languages.css({
+    'left': '100%',
+    opacity: '0'
 });
+languages.waypoint(function() {
+    languages.animate({
+        'left': '50%',
+        opacity: '1'
+    }, 2000, 'easeOutQuart');
+}, { offset : '75%' });
+
+
+//      [[---  languages list animation  ---]]
+const languages_list = $('.languages').find('ul');
+languages_list.css({
+    'height': '1.6em'
+});
+const languages_list_items = languages_list[0].children.length;
+languages_list.waypoint(function() {
+    languages_list.delay(1000).animate({
+        'height': `${languages_list_items * 1.6}em`
+    }, 2000, 'easeOutQuart');
+}, { offset : '75%' });
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,33 @@
 var fileExt = ".svg";
 let fileUrl = "../assets/images/background-images/"
 
-$(document).ready(
-function(){
-    $.ajax({
-        //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-        url: fileUrl,
-        success: function (data) {
-            $(data).find('a:contains(" + ${fileExt} + ")').each(function () {
-                var filename = this.href.replace(window.location, "").replace("http:///", "");
-                console.log(filename);
-            });
-        }     
+let validImages = [];
+
+
+$.get(fileUrl, function(data) {
+    let filteredData = $('div', data)['prevObject'].find('li').find('span.name').filter(function() {
+        // filter out all the files that don't end in .svg
+        return this.textContent.match(fileExt);
     });
+
+    // loop through the data and add the text to a array
+    filteredData.each(function() {
+        validImages.push(this.textContent);
+    });
+    
+}).done(function() {
+    // set the background image to a random image
+    let randomImage = validImages[Math.floor(Math.random() * validImages.length)];
+    $('.container').css('background-image', `url('../assets/images/background-images/${randomImage}')`);
+});
+
+
+// when clicking on an image open image viewer
+$('img').click(function() {
+    let imageUrl = $(this).attr('src');
+    $('.img-viewer').removeClass('hidden');
+    $('#img-viewer-image').attr('src', imageUrl);
+});
+$('.img-viewer').click(function() {
+    $(this).addClass('hidden');
 });
