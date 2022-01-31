0c7b410f9a444a3a16c9ff064d3aa9f3b562fc34
RalkeyOfficial <Ralkey@outlook.com>
2022-01-31 14:02:23 +0100

    added font, created simple header, created files, typewriter now works

--- V2/assets/js/script.js
+++ V2/assets/js/script.js
@@ -1,7 +1,9 @@
 
-// tickerText(contents, keep, seconds, delay = 20, iterations = 0, ratio, secondsout, dev = false, pausetarget, stoptarget);
-let $elements = $(".tt-holder .tt");
-let animatetext = function () {
-    $(".ttt").tickerText($elements, 16, 3, 100, 1, 0.5, 2, false, "timerpause", "timerstop");
-};
-$(animatetext);
+$(document).ready(function () {
+    // tickerText(contents, keep, seconds, delay = 20, iterations = 0, ratio, secondsout, dev = false, pausetarget, stoptarget);
+    let $elements = $(".tt-holder .tt");
+    let animatetext = function () {
+        $(".ttt").tickerText($elements, 6, 1, 100, 0, 0.5, 2, false, "timerpause", "timerstop");
+    };
+    $(animatetext);
+});
